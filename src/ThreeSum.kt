eb76aa3201355114a12dad8b73082aeb76070bc7
latte-kim <latte.kim@kakaocorp.com>
2019-01-02 09:27:58 +0900

    ThreeSum 개선 및 주석 추가

--- src/ThreeSum.kt
+++ src/ThreeSum.kt
@@ -28,7 +28,7 @@ fun main(args: Array<String>) {
 
     }
     println("///")
-    threeSum(intArrayOf(1,1,-2)).forEach { list ->
+    threeSum(intArrayOf(1, 1, -2)).forEach { list ->
         list.forEach {
             print("$it ")
         }
@@ -52,7 +52,7 @@ fun main(args: Array<String>) {
     }
     println("///")
 
-    threeSum(intArrayOf(0,0,0,0)).forEach { list ->
+    threeSum(intArrayOf(0, 0, 0, 0)).forEach { list ->
         list.forEach {
             print("$it ")
         }
@@ -3079,26 +3079,31 @@ fun threeSum(nums: IntArray): List<List<Int>> {
 
     val result: MutableList<List<Int>> = mutableListOf()
 
+    //i를 기준으로 i+1 부터 nums.size-1까지의 리스트에서 가장 왼쪽, 가장 오른쪽으로 부터 가운데로 좁혀지면서 문제를 해결한다 (정렬되어 있다는 기준 하에)
     for (i in 0 until nums.size) {
 
-        if(i>0 && nums[i] == nums[i-1]){
+        //nums[i]와 nums[i+1]이 아닌 nums[i-1]을 비교하는 바는 전체가 왼쪽부터 오른쪽으로 훑기 시작하므로 -1, -1, -1, 2, 3과 같은 배열이 있을 때 가장 많은 선택지를 가질 수 있는 것은 제일 왼쪽의 숫자이기 때문이다.
+        if (i > 0 && nums[i] == nums[i - 1]) {
             continue
         }
-        var left = i+1
-        var right = nums.size-1
+        var left = i + 1
+        var right = nums.size - 1
 
-        while(left<right){
+        while (left < right) {
             val sum = nums[i] + nums[left] + nums[right]
-            if(sum>0){
+            if (sum > 0) {
                 right--
-            }else if( sum < 0){
+            } else if (sum < 0) {
                 left++
-            }else{
-                result.add(result.size, listOf(nums[i],nums[left],nums[right]))
-                while(left<right && nums[left]== nums[left+1]) {
+            } else {
+                result.add(result.size, listOf(nums[i], nums[left], nums[right]))
+                //같은 값이라면 가장 오른쪽에 있는 값을 선택한다(왼쪽에서 선택하면 다시 진행방향에 따라 같은 값이 다시 선택 될 수 있으므로
+                while (left < right && nums[left] == nums[left + 1]) {
                     left++
                 }
-                while(left<right && nums[right]== nums[right-1]){
+
+                //위와 마찬가지
+                while (left < right && nums[right] == nums[right - 1]) {
                     right--
                 }
                 left++
@@ -3109,6 +3114,7 @@ fun threeSum(nums: IntArray): List<List<Int>> {
     }
 
     return result
+
 }
 
 /*
@@ -3231,4 +3237,5 @@ fun threeSum(nums: IntArray): List<List<Int>> {
     if (zeroCnt>=3) {
         result.add(result.size,listOf(0, 0, 0))
     }
-    return result*/
+    return result
+}*/
